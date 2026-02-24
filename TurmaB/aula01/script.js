854c93987d56a6e6222b8eaa0dc21fa3db31ed52
Guilherme Henrique de Souza <guilherme.souza@etec.sp.gov.br>
2026-02-24 11:40:28 -0300

    24/2

---
+++ TurmaB/aula01/script.js
@@ -0,0 +1,11 @@
+var nome = "Guilherme";
+var idade = 46;
+var altura = 1.82;
+
+console.log( nome );
+console.log( idade );
+console.log( altura );
+
+document.write( nome );
+document.write( idade );
+document.write( altura );

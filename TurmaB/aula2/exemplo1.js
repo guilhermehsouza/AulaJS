81d83f550b2c77c42fcc2a827f54d300f113f96c
Guilherme Henrique de Souza <guilhermehsouza16@gmail.com>
2026-03-25 14:52:49 -0300

    25/3

---
+++ TurmaB/aula2/exemplo1.js
@@ -0,0 +1,13 @@
+function calcular()
+{
+    let nome, idade, dias;
+
+    nome = document.getElementById("aluno").value;
+    idade = Number( document.getElementById("idade").value);
+
+    dias = idade * 365;
+
+    document.getElementById("resultado").innerHTML = 
+       "<p> O aluno " + nome + " já viveu " + dias + " dias.";
+
+}
